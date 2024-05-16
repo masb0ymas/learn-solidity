b785f39dc702b28b373b39aeeb030234bf7b7cd7
masb0ymas <n.fajri@outlook.com>
2024-05-16 20:59:02 +0700

    feat: add lottery contract

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
+const inboxPath = path.resolve(__dirname, "contracts", "lottery.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
 const input = {
   language: "Solidity",
   sources: {
-    "inbox.sol": {
+    "lottery.sol": {
       content: source,
     },
   },
@@ -23,4 +23,6 @@ const input = {
 
 const solCompile = JSON.parse(solc.compile(JSON.stringify(input)));
 
-module.exports = solCompile.contracts["inbox.sol"].Inbox;
+console.log(inboxPath, source, input, solCompile)
+
+module.exports = solCompile.contracts["lottery.sol"].Lottery;
---
+++ test/lottery.test.js
@@ -0,0 +1,40 @@
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const { abi, evm } = require("../compile");
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Lottery contract", () => {
+  it("deploy a contract", () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it("allows one account to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+});
